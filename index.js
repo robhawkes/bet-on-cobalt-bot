b3c8e9b4235d330a6921c55b2b217d7e23691eba
Robin Hawkes <rob@rawkes.com>
2016-01-27 20:43:12 +0000

    Full on spread betting

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// TODO: Bots switch at half-time so look at trends in odds / wins to get a
+// better idea of which team is doing well most recently
+
 var irc = require('tmi.js');
 var chalk = require('chalk');
 
@@ -38,13 +41,82 @@ var clientWhisper = new irc.client(optionsWhisper);
 
 var betCount = 0;
 
-var placeBet = function() {
+var parseOdds = function(oddsStr) {
+  var results = oddsStr.match(/Red:(\d*.\d*)|Blue:(\d*.\d*)|Purple:(\d*.\d*)|Green:(\d*.\d*)|Multi:(\d*.\d*)|Ace:(\d*.\d*)|Whole:(\d*.\d*)|Combo:(\d*.\d*)/gi);
+
+  var odds = {};
+  var oddSplit;
+  results.forEach(function(odd) {
+    oddSplit = odd.split(':');
+    odds[oddSplit[0].toLowerCase()] = Number(oddSplit[1]);
+  });
+
+  return odds;
+};
+
+var placeBets = function(oddsStr) {
   if (betCount === 0) {
     setTimeout(getVolts, 1000 + (Math.random() * 4000));
   }
 
-  console.log(chalk.blue('Placing bet...'));
-  clientWhisper.whisper('bot_cobalt', '!b c 20%');
+  var odds = parseOdds(oddsStr);
+
+  console.log(chalk.blue('Placing bets...'));
+
+  console.log(chalk.gray.bgBlue('Betting multi 1%'));
+  clientWhisper.whisper('bot_cobalt', '!b m 1%');
+
+  setTimeout(function() {
+    console.log(chalk.gray.bgBlue('Betting ace 1%'));
+    clientWhisper.whisper('bot_cobalt', '!b a 1%');
+  }, 1000);
+
+  setTimeout(function() {
+    console.log(chalk.gray.bgBlue('Betting whole 1%'));
+    return clientWhisper.whisper('bot_cobalt', '!b w 1%');
+  }, 2000);
+
+  setTimeout(function() {
+    console.log(chalk.gray.bgBlue('Betting combo 5%'));
+    return clientWhisper.whisper('bot_cobalt', '!b c 5%');
+  }, 3000);
+
+  var oddsByTeam = [];
+
+  if (odds.red) {
+    oddsByTeam.push([odds.red, 'r', 'red']);
+  }
+
+  if (odds.blue) {
+    oddsByTeam.push([odds.blue, 'b', 'blue']);
+  }
+
+  if (odds.purple) {
+    oddsByTeam.push([odds.purple, 'p', 'purple']);
+  }
+
+  if (odds.green) {
+    oddsByTeam.push([odds.green, 'g', 'green']);
+  }
+
+  // Sort teams by odds
+  oddsByTeam.sort(function(a, b) {
+    if (a[0] < b[0]) {
+      return -1;
+    }
+
+    if (a[0] > b[0]) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  // Bet on team with best odds
+  setTimeout(function() {
+    console.log(chalk.gray.bgBlue('Betting ' + oddsByTeam[0][2] + ' 10%'));
+    clientWhisper.whisper('bot_cobalt', '!b ' + oddsByTeam[0][1] + ' 10%');
+  }, 4000);
 
   betCount++;
 
@@ -74,9 +146,10 @@ clientWhisper.connect().then(function() {
 // Participation Bonus: Match Over +100 to all viewers!
 // Volts: rocketscientists:76263, grum_:101, iscre4m:113384, shoghicp:8470, profmobius:187446
 
-var regexRound = /(.*)winning bets(.*)/i;
-var regexBets = /(.*)place your bets(.*)/i;
-var regexVolts = new RegExp(config.twitch.user + ':(\d*)', 'i');
+var regexRound = /.*winning bets.*/i;
+var regexMatch = /.*match over.*/i;
+var regexBets = /.*place your bets\! (.*)/i;
+var regexVolts = new RegExp(config.twitch.user + ':(\\d*)', 'i');
 
 clientChat.on('action', function (channel, user, message, self) {
   if (!user || user.username != 'bot_cobalt') {
@@ -87,10 +160,17 @@ clientChat.on('action', function (channel, user, message, self) {
     console.log(chalk.gray.bgYellow(message));
   }
 
-  if (regexBets.exec(message)) {
+  if (regexMatch.exec(message)) {
+    console.log(chalk.gray.bgRed(message));
+  }
+
+  var odds = regexBets.exec(message);
+  if (odds) {
     console.log(chalk.gray.bgYellow(message));
-    console.log(chalk.blue('Placing bet after cooldown period...'));
-    setTimeout(placeBet, 5000 + (Math.random() * 5000));
+    console.log(chalk.blue('Placing bets after cooldown period...'));
+    setTimeout(function() {
+      placeBets(odds[1]);
+    }, 5000 + (Math.random() * 5000));
   }
 
   if (message.startsWith('Volts:')) {
